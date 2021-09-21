3018a3e4ad19c6ec69382649328ee520fe0358c3
NicklasRitola <nicke_ritola@hotmail.com>
2021-09-21 22:00:58 +0200

    Added some comments
    Potential solutions to how to render different buttons depending on login type.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,9 +3,11 @@ var path = require('path');
 const user_home = function(request, response) { 
 
 	var firstName = request.session.firstName;
+	/*Potentiell lösning för att rendera olika knappar beroende på inloggning
+	var role = request.session.role; */
 
 	if (request.session.role === "client") {
-		
+		/*Skicka med role som parameter till redirecten, och rendera olika knappar beroende på rollen */
 		response.render(path.join(__dirname, "../views/clientViews/clientHome"), {firstName: firstName})
 
 	} else if (request.session.role === "trainer") {
@@ -20,4 +22,3 @@ const user_home = function(request, response) {
 module.exports = {
 	user_home
 }
-
