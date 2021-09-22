dc31ba05dbf5f80ebf6af5aad4eb86107f492550
yadkhorshid <83356924+yadkhorshid@users.noreply.github.com>
2021-09-22 13:30:49 +0200

    Revert "Removed dbConfig from git file tracking"
    This reverts commit fd963516a5b145e6a56766fb14087ec9b34f3231.

---
+++ dbConfig/dbConfig.js
@@ -0,0 +1,35 @@
+/*
+This file is used to setup the database connection and share it with other files. 
+Other files can access the database by importing this JS file. 
+Might need to add this to a gitIgnore file, since everyone has different root-passwords.
+*/
+
+var mysql = require("mysql2");
+
+var connection = {
+	host     : 'localhost',
+	user     : 'root',
+	password : '12345',
+	database : 'workout_db',
+  connectionLimit: 5 
+};
+
+var dbconnection = mysql.createPool(
+  connection
+);
+
+dbconnection.on('connection', function (connection) {
+
+  console.log('DB Connection established');
+
+  connection.on('error', function (err) {
+    console.error(new Date(), 'MySQL error', err.code);
+  });
+  connection.on('close', function (err) {
+    console.error(new Date(), 'MySQL close', err);
+  });
+
+});
+
+module.exports = dbconnection;
+
