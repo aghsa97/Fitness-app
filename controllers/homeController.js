c4b97fa0bce17209c0012c58a5345626c6444d8e
aghsa97 <83072153+aghsa97@users.noreply.github.com>
2021-09-23 19:12:10 +0200

    Client home page updates

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -11,7 +11,7 @@ const user_home = function(request, response) {
 	
 	if (request.session.role === "client") {
 		var sql_friends = 
-		`SELECT ui.firstname, ui.lastname, user.email
+		`SELECT ui.firstname, ui.lastname, user.email, ui.role
 		FROM user_info AS ui
 		join user on user.id = ui.id
 		WHERE user_id IN (SELECT r_friend_id FROM user_friends WHERE a_friend_id = ? and accepted = 1) 
